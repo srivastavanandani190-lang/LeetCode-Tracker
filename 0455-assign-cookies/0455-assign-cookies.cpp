325bac8381874ab7ec133b9ecc37f2f9dc34156a
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-03-20 09:12:05 +0530

    Time: 15 ms (16.03%), Space: 44.8 MB (77.92%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        // sort(g.begin(), g.end());
-        // sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
         int i = 0;  
         int j = 0;  
