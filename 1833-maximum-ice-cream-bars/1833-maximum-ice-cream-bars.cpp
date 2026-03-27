2610cf1f99fecc4c256eda788b98663041a8a01d
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-03-27 09:13:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int maxCost = 0;
-        
-        // Step 1: Find maximum cost
-        for (int cost : costs) {
-            maxCost = max(maxCost, cost);
-        }
-        
-        // Step 2: Create frequency array
-        vector<int> freq(maxCost + 1, 0);
-        for (int cost : costs) {
-            freq[cost]++;
-        }
+        sort(costs.begin(), costs.end());  // sort prices
         
         int count = 0;
         
-        // Step 3: Buy from cheapest to costliest
-        for (int price = 1; price <= maxCost; price++) {
-            while (freq[price] > 0 && coins >= price) {
-                coins -= price;
-                freq[price]--;
+        for (int cost : costs) {
+            if (coins >= cost) {
+                coins -= cost;
                 count++;
+            } else {
+                break;
             }
         }
         
