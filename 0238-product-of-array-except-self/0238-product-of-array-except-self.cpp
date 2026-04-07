eed5c0909c7f3a970ae40a3f934385570ac70f1e
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-04-07 09:57:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (35.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,18 +2,35 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n, 1);
+        vector<int> result;
 
-    
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            result[i] = prefix;
-            prefix *= nums[i];
+        int pro = 1;
+        int zeroCount = 0;
+
+        // Step 1: Calculate product of non-zero elements & count zeros
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                zeroCount++;
+            } else {
+                pro *= nums[i];
+            }
         }
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= suffix;
-            suffix *= nums[i];
+
+        // Step 2: Build result based on zero count
+        for(int i = 0; i < n; i++){
+            if(zeroCount > 1){
+                result.push_back(0);
+            }
+            else if(zeroCount == 1){
+                if(nums[i] == 0){
+                    result.push_back(pro); // only zero gets product
+                } else {
+                    result.push_back(0);
+                }
+            }
+            else{
+                result.push_back(pro / nums[i]);
+            }
         }
 
         return result;
