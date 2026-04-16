36f9f6f0fe9bca4b777cdb58bb4396ed9457a141
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-04-16 12:16:23 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result;
+        
+    int n = nums.size();
+    vector<int> ans(n, 1);
 
-        int pro = 1;
-        int zeroCount = 0;
-
-        // Step 1: Calculate product of non-zero elements & count zeros
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                zeroCount++;
-            } else {
-                pro *= nums[i];
-            }
-        }
-
-        // Step 2: Build result based on zero count
-        for(int i = 0; i < n; i++){
-            if(zeroCount > 1){
-                result.push_back(0);
-            }
-            else if(zeroCount == 1){
-                if(nums[i] == 0){
-                    result.push_back(pro); // only zero gets product
-                } else {
-                    result.push_back(0);
-                }
-            }
-            else{
-                result.push_back(pro / nums[i]);
-            }
-        }
-
-        return result;
+    // Prefix
+    for(int i = 1; i < n; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
+
+    // Suffix
+    int suffix = 1;
+    for(int i = n - 1; i >= 0; i--) {
+        ans[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return ans;
+}
 };
 
