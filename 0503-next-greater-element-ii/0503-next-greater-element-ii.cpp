2ee0bdee9b652141efb5a2c605da149c7225e7f8
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-04-18 19:41:26 +0530

    Time: 121 ms (5.07%), Space: 27.8 MB (97.51%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, -1);
+
+        for(int i = 0; i < n; i++){ 
+            for(int j = 1; j < n; j++){
+                int nextIndex = (i + j) % n;
+                
+                if(nums[nextIndex] > nums[i]){
+                    res[i] = nums[nextIndex];
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+};
