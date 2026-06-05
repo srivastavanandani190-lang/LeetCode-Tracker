53337020291140088609ebfd9debe3206b50533f
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-05 17:05:08 +0530

    Time: 0 ms (100%), Space: 20.6 MB (82.87%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
-        for(int num : nums) {
-            result ^= num;  
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
         }
-        return result;
+        return nums[n-1];
     }
 };
