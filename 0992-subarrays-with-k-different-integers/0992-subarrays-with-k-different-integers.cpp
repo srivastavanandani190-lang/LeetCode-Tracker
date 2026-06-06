ee12f71accb55a489d9d10aad63a7db98b1a8540
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-06 13:42:14 +0530

    Time: 47 ms (90.57%), Space: 51 MB (91.15%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -4,39 +4,39 @@ public:
         int t=0;
         unordered_map<int,int>freq;
         int s=0,e=0,n=nums.size(),count=0,total=0;
-        while(e<n){
+            while(e<n){
+            if(freq[nums[e]]==0)
+            //encuntered a new element so increase count by one
+            count++;
             freq[nums[e]]++;
-            if(freq[nums[e]]==1){
-                count++;
-            }
-            while(count==k){
-                total+=n-e;
+            //count>k, so start decreasing our window
+            while(count>k){
                 freq[nums[s]]--;
-                if(freq[nums[s]]==0){
-                    count--;
-                }
+                if(freq[nums[s]]==0)
+                count--;
                 s++;
-
             }
+            //count<k, so add it in our answer
+            total+=e-s+1;
             e++;
         }
         s=0,e=0,count=0;
         freq.clear();
-        k++;
+        k--;
          while(e<n){
+            if(freq[nums[e]]==0)
+            //encuntered a new element so increase count by one
+            count++;
             freq[nums[e]]++;
-            if(freq[nums[e]]==1){
-                count++;
-            }
-            while(count==k){
-                total-=n-e;
+            //count>k, so start decreasing our window
+            while(count>k){
                 freq[nums[s]]--;
-                if(freq[nums[s]]==0){
-                    count--;
-                }
+                if(freq[nums[s]]==0)
+                count--;
                 s++;
-
             }
+            //count<k, so add it in our answer
+            total-=e-s+1;
             e++;
         }
         return total;
