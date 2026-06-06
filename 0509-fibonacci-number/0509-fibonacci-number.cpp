e42366b4c3323ac5fa7129f170a74dcfe6f4d361
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-06 16:43:18 +0530

    Time: 16 ms (21.69%), Space: 8.2 MB (11.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0 || n==1){
+     int fibon(int n, vector<int>&dp){
+        if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return fibon(n-1,dp)+fibon(n-2,dp);
+     }
+    int fib(int n) {
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // return fib(n-1)+fib(n-2);
+        vector<int>dp(n+1,-1);
+        return fibon(n,dp);
     }
 };
