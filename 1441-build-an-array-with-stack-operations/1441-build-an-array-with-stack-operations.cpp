95633b6afa7d78db86dfd2fc4abb0146e299ecd3
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-12 11:29:47 +0530

    Time: 0 ms (100%), Space: 10.9 MB (19.45%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int j = 0;  // pointer for target
+
+        for (int i = 1; i <= n && j < target.size(); i++) {
+            ans.push_back("Push");
+
+            if (i == target[j]) {
+                j++;   // keep this element
+            } else {
+                ans.push_back("Pop"); // remove unnecessary element
+            }
+        }
+
+        return ans;
+    }
+};
