c688cfd8f062d842740e3edba2f07d2cdd4d3586
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-20 13:28:34 +0530

    Time: 3 ms (1.38%), Space: 10.2 MB (19.17%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,42 @@
+class MyQueue {
+    stack<int> inStack;
+    stack<int> outStack;
+
+    void transfer() {
+        while (!inStack.empty()) {
+            outStack.push(inStack.top());
+            inStack.pop();
+        }
+    }
+
+public:
+    MyQueue() {
+        
+    }
+
+    void push(int x) {
+        inStack.push(x);
+    }
+
+    int pop() {
+        if (outStack.empty()) {
+            transfer();
+        }
+
+        int val = outStack.top();
+        outStack.pop();
+        return val;
+    }
+
+    int peek() {
+        if (outStack.empty()) {
+            transfer();
+        }
+
+        return outStack.top();
+    }
+
+    bool empty() {
+        return inStack.empty() && outStack.empty();
+    }
+};
