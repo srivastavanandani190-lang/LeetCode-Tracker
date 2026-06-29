18ae1eef2562d3ac3dccc35cd6939179ef620876
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-29 13:21:26 +0530

    Time: 0 ms (100%), Space: 10 MB (12.95%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>p;
+        for(int i=0;i<stones.size();i++){
+            p.push(stones[i]);
+        }
+        while(p.size()>1){
+            int w=p.top();
+            p.pop();
+            w-=p.top();
+            p.pop();
+            if(w){
+                p.push(w);
+            }
+        }
+        return p.empty()? 0:p.top();
+    }
+};
