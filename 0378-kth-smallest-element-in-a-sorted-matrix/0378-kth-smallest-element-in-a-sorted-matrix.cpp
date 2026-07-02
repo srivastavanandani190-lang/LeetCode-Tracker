0d445866ca89afced858adc82c07947d7a664ce9
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-02 11:44:37 +0530

    Time: 9 ms (30.63%), Space: 17.5 MB (56.13%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
+        vector<pair<int, pair<int,int>>> temp;
 
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (pq.size() < k) {
-                    pq.push(matrix[i][j]);
-                } else if (matrix[i][j] < pq.top()) {
-                    pq.pop();
-                    pq.push(matrix[i][j]);
-                }
+        for(int i = 0; i < matrix.size(); i++) {
+            temp.push_back({matrix[i][0], {i, 0}});
+        }
+
+        priority_queue<
+            pair<int,pair<int,int>>,
+            vector<pair<int,pair<int,int>>>,
+            greater<pair<int,pair<int,int>>>
+        > p(temp.begin(), temp.end());
+
+        int ans = 0;
+
+        while(k--) {
+
+            auto element = p.top();
+            p.pop();
+
+            ans = element.first;
+
+            int i = element.second.first;
+            int j = element.second.second;
+
+            if(j + 1 < matrix[i].size()) {
+                p.push({matrix[i][j + 1], {i, j + 1}});
             }
         }
 
-        return pq.top();
+        return ans;
     }
 };
+   
