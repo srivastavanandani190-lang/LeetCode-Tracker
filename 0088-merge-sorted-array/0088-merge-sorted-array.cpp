25a9b0250bb8cf53349b30df3a5b687838bea407
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-04 14:55:12 +0530

    Time: 0 ms (100%), Space: 12.8 MB (9.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> res;
 
-        for(int i = 0; i < m; i++) {
-            res.push_back(nums1[i]);
+        vector<vector<int>> A;
+
+        vector<int> a, b;
+
+        for (int i = 0; i < m; i++)
+            a.push_back(nums1[i]);
+
+        for (int i = 0; i < n; i++)
+            b.push_back(nums2[i]);
+
+        A.push_back(a);
+        A.push_back(b);
+
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq;
+
+        int k = A.size();
+
+        // Push first element of each array
+        for (int i = 0; i < k; i++) {
+            if (!A[i].empty()) {
+                pq.push({A[i][0], {i, 0}});
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            res.push_back(nums2[i]);
-        }
+        int idx = 0;
 
-        sort(res.begin(), res.end());
+        while (!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
 
-         
-        for(int i = 0; i < m + n; i++) {
-            nums1[i] = res[i];
+            int val = cur.first;
+            int row = cur.second.first;
+            int col = cur.second.second;
+
+            nums1[idx++] = val;
+
+            if (col + 1 < A[row].size()) {
+                pq.push({A[row][col + 1], {row, col + 1}});
+            }
         }
     }
 };
