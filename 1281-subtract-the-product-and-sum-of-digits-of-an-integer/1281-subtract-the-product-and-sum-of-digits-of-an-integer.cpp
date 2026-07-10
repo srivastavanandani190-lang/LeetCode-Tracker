8dd256761f094e568176f3b4c6cd359e7eb52bf7
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-10 12:27:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (54.59%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -3,12 +3,13 @@ public:
     int subtractProductAndSum(int n) {
         int pro=1;
         int sum=0;
-        int rem=0;
+     
         while(n!=0){
-            rem=n%10;
-            pro=pro*rem;
-            sum+=rem;
-            n=n/10;        }
-return pro-sum;
+             
+            pro*=n%10;
+            sum+=n%10;
+            n=n/10;       
+             }
+          return pro-sum;
     }
 };
