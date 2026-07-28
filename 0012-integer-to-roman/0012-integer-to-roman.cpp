661ea0309f41c439201f6c7d3af2e1dd057c542c
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-28 22:01:07 +0530

    Time: 8 ms (18.38%), Space: 12.7 MB (18.89%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> roman = {
-            {1000, "M"},
-            {900, "CM"},
-            {500, "D"},
-            {400, "CD"},
-            {100, "C"},
-            {90, "XC"},
-            {50, "L"},
-            {40, "XL"},
-            {10, "X"},
-            {9, "IX"},
-            {5, "V"},
-            {4, "IV"},
+        // Pairs of values and their corresponding Roman symbols in descending order
+        vector<pair<int, string>> romanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
             {1, "I"}
         };
-
+        
         string result = "";
-
-        for (auto &p : roman) {
-            while (num >= p.first) {
-                result += p.second;
-                num -= p.first;
+        
+        for (const auto& [value, symbol] : romanMap) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
             }
         }
-
+        
         return result;
     }
 };
