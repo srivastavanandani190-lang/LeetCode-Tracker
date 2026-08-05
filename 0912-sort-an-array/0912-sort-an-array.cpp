e1f116c6815fd4ae6e1ecb72b562e448249a542d
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-05 17:45:05 +0530

    Time: 48 ms (75.41%), Space: 70.9 MB (85.51%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,8 @@
+#include <vector>
+
 class Solution {
 public:
-    void heapify(vector<int>& arr, int n, int i) {
+    void heapify(std::vector<int>& arr, int n, int i) {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
@@ -12,28 +14,25 @@ public:
             largest = r;
 
         if (largest != i) {
-            swap(arr[i], arr[largest]);
+            std::swap(arr[i], arr[largest]);
             heapify(arr, n, largest);
         }
     }
 
-    void BuildMaxHeap(vector<int>& arr, int n) {
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
-    }
-
-    void sortarray(vector<int>& arr, int n) {
-        for (int i = n - 1; i > 0; i--) {
-            swap(arr[0], arr[i]);
-            heapify(arr, i, 0);
-        }
-    }
-
-     vector<int> sortArray(vector<int>& nums) {
+    std::vector<int> sortArray(std::vector<int>& nums) {
         int n = nums.size();
-        BuildMaxHeap(nums, n);
-        sortarray(nums, n);
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        // Extract elements one by one from heap
+        for (int i = n - 1; i > 0; i--) {
+            std::swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+
         return nums;
     }
 };
