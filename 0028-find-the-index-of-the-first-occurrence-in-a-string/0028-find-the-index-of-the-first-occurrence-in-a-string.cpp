19bcfa1c6462bd2675812912abd0349dbd50a935
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-13 09:40:54 +0530

    Time: 29 ms (10.41%), Space: 72.7 MB (5%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,71 +6,23 @@
 // };
 class Solution {
 public:
- void findLPS(vector<int>& lps, string& pattern) {
-        int pre = 0;
-        int suf = 1;
-
-        while (suf < pattern.size()) {
-            if (pattern[pre] == pattern[suf]) {
-                lps[suf] = pre + 1;
-                pre++;
-                suf++;
-            } else {
-                if (pre == 0) {
-                    lps[suf] = 0;
-                    suf++;
-                } else {
-                    pre = lps[pre - 1];
-                }
-            }
-        }
-    }
+  
     int strStr(string haystack, string needle) {
-        // int n = haystack.length();
-        // int m = needle.length();
- 
-        // if (m == 0) return 0;
-        // if (m > n) return -1;
-
-     
-        // for (int i = 0; i <= n - m; i++) {
-        //     int j;
-        //     for (j = 0; j < m; j++) {
-        //         if (haystack[i + j] != needle[j]) {
-        //             break;
-        //         }
-        //     }
- 
-        //     if (j == m) {
-        //         return i;
-        //     }
-        // }
-
-        // return -1;
-        if (needle.empty())
-            return 0;
-
-        vector<int> lps(needle.size(), 0);
-        findLPS(lps, needle);
-
-        int first = 0, second = 0;
-
-        while (first < haystack.size() && second < needle.size()) {
-            if (haystack[first] == needle[second]) {
-                first++;
-                second++;
-            } else {
-                if (second == 0) {
-                    first++;
-                } else {
-                    second = lps[second - 1];
-                }
+      // code here
+       int n = haystack.size();
+        int m = needle.size();
+        
+         
+        if (m > n) return -1;
+        
+        
+        for (int i = 0; i <= n ; i++) {
+            
+            if (haystack.substr(i, m) == needle) {
+                return i;  
             }
         }
-
-        if (second == needle.size())
-            return first - second;
-
-        return -1;
+        
+        return -1; 
     }
 };
