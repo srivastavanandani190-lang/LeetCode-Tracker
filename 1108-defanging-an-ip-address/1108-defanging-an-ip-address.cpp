1802b4b61e0abf0fbdec69cce5f5b85eda7316ab
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-13 22:57:19 +0530

    Time: 2 ms (54.59%), Space: 7.7 MB (49.17%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        int index=0;
-        string ans;
-        while(index<address.size()){
-            if(address[index]=='.'){
-                ans+="[.]";
-            }
-            else{
-                ans+=address[index];
-            }
-            index++;
+       int n=address.size();
+       string ans;
+       for(int i=0;i<n;i++){
+        if(address[i]=='.'){
+            ans+="[.]";
         }
+        else{
+            ans+=address[i];
+        }
+       }
         return ans;
     }
 };
