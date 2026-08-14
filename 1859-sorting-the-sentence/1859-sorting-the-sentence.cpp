79225a64091f74ae514130e46686826a312b85c1
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-14 23:41:15 +0530

    Time: 3 ms (41.94%), Space: 8.5 MB (42.54%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,35 +1,59 @@
+// class Solution {
+// public:
+//     string sortSentence(string s) {
+//         vector<string> ans(10);
+//         string temp;
+//         int count = 0, index = 0;
+
+//         while (index < s.size()) {
+//             if (s[index] == ' ') {
+//                 int pos = temp.back() - '0';
+//                 temp.pop_back();
+//                 ans[pos] = temp;
+//                 temp.clear();
+//                 count++;
+//             } else {
+//                 temp += s[index];
+//             }
+//             index++;
+//         }
+
+//         int pos = temp.back() - '0';
+//         temp.pop_back();
+//         ans[pos] = temp;
+
+//         string res;
+
+//         for (int i = 1; i <= count + 1; i++) {
+//             res += ans[i];
+//             if (i != count + 1)
+//                 res += " ";
+//         }
+
+//         return res;
+//     }
+// };
 class Solution {
 public:
     string sortSentence(string s) {
-        vector<string> ans(10);
-        string temp;
-        int count = 0, index = 0;
+        pair<int,string> p[10];
+        string w, ans;
 
-        while (index < s.size()) {
-            if (s[index] == ' ') {
-                int pos = temp.back() - '0';
-                temp.pop_back();
-                ans[pos] = temp;
-                temp.clear();
-                count++;
+        for(char c : s + ' ') {
+            if(c == ' ') {
+                int pos = w.back() - '0';
+                w.pop_back();
+                p[pos] = {pos, w};
+                w.clear();
             } else {
-                temp += s[index];
+                w += c;
             }
-            index++;
         }
 
-        int pos = temp.back() - '0';
-        temp.pop_back();
-        ans[pos] = temp;
+        for(int i = 1; i < 10; i++)
+            if(!p[i].second.empty())
+                ans += (ans.empty() ? "" : " ") + p[i].second;
 
-        string res;
-
-        for (int i = 1; i <= count + 1; i++) {
-            res += ans[i];
-            if (i != count + 1)
-                res += " ";
-        }
-
-        return res;
+        return ans;
     }
 };
