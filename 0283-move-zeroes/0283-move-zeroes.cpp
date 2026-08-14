9b96a61914773d0565a1979f8325b82bf9c1aa30
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-14 13:50:51 +0530

    Time: 10 ms (7.54%), Space: 24 MB (19.8%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;  // index for non-zero elements
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
+        int n = nums.size();   
+        
+        // Notice there is no i++ here in the loop definition
+        for(int i = 0; i < n; ){ 
+            if(nums[i] == 0){
+                nums.push_back(0);
+                nums.erase(nums.begin() + i);
+                
+                // Shrink the boundary so we don't process the zeroes we just added to the back
+                n--; 
+            } else {
+                // Only move to the next index if we DID NOT erase an element
+                i++; 
             }
         }
     }
