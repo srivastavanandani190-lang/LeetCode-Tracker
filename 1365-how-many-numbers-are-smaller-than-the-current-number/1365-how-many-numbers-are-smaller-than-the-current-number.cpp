b2311d7de964a4c3ce07f501411027994ffba83c
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-14 14:10:13 +0530

    Time: 6 ms (54.99%), Space: 14.5 MB (18.42%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
+        // vector<int> sorted = nums;
+        // sort(sorted.begin(), sorted.end());
 
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
 
-        for (int i = 0; i < sorted.size(); i++) {
-            if (mp.find(sorted[i]) == mp.end()) {
-                mp[sorted[i]] = i;  // first occurrence
-            }
-        }
+        // for (int i = 0; i < sorted.size(); i++) {
+        //     if (mp.find(sorted[i]) == mp.end()) {
+        //         mp[sorted[i]] = i;  // first occurrence
+        //     }
+        // }
 
+        // vector<int> ans;
+        // for (int num : nums) {
+        //     ans.push_back(mp[num]);
+        // }
+
+     vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        
         vector<int> ans;
-        for (int num : nums) {
-            ans.push_back(mp[num]);
+        
+        // Step 2: Use lower_bound to find the count of smaller numbers
+        for(int i = 0; i < nums.size(); i++){
+            // lower_bound returns an iterator. Subtracting the begin() iterator 
+            // gives the integer index/count of smaller elements.
+            int count = lower_bound(sorted_nums.begin(), sorted_nums.end(), nums[i]) - sorted_nums.begin();
+            ans.push_back(count);
         }
-
+        
         return ans;
     }
 };
