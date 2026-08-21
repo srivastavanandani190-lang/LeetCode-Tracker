2fe405c277d2fd437e54800f5803c0b7008834b7
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-21 14:00:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.83%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -12,7 +12,7 @@ public:
             }
             else if(command[i]=='('&& command[i+1]=='a'&& command[i+2]=='l'&& command[i+3]==')') {
                 ans+="al";
-                i=i+1;
+                i=i+3;
             }
         }
         return ans;
