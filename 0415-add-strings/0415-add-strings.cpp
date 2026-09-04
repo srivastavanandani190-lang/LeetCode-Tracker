08b03202b1b970e96ef2a80eaeaf5210b6e9d9f6
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-09-04 15:19:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (23.69%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
- string add(string nums1, string nums2){
-    string ans;
-    int i1=nums1.size()-1;
-    int i2=nums2.size()-1;
-    int carry=0,sum;
-    while(i2>=0){
-        sum=(nums1[i1]-'0')+(nums2[i2]-'0')+carry;
-        carry=sum/10;
-        char c='0'+(sum%10);
-        ans+=c;
-        i1--;
-        i2--;
-    }
-    while(i1>=0){
-        sum=(nums1[i1]-'0')+carry;
-        carry=sum/10;
-        char c='0'+(sum%10);
-        ans+=c;
-        i1--;
-    }
-    if(carry){
-        ans+='1';
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
- }
     string addStrings(string num1, string num2) {
-       if(num1.size()<num2.size()){
-        return add(num2,num1);
-       }
-       else{
-        return add(num1,num2);
-       }
-       
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string res = "";
+
+        // Loop continues if there's a digit in num1, num2, or a leftover carry
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += num1[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2[j] - '0';
+                j--;
+            }
+
+            carry = sum / 10;
+            res += to_string(sum % 10);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
